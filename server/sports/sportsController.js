32b7be631c51e90fec047e930fc76d7f08727ffc
jimjea <jimjea@gmail.com>
2014-09-20 16:42:09 -0700

    modular controllers

--- server/sports/sportsController.js
+++ server/sports/sportsController.js
@@ -1,1 +1,15 @@
-// contains sports API logic
+// sports API handled here
+var NFL = require('sportsdata');
+
+var apiKey = 'gzwk495a6tuuhjsg2zk5sumd'; // will store this is separate keys file
+
+var NFLinit = NFL.init('t', 1, )
+
+module.exports = {
+  test: function(req, res) {
+    res.redirect('/');
+    console.log('worked')
+  },
+
+
+};

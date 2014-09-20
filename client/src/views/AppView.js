cd5b88df2fafb97c3a2c30982e497b78cc117199
sputh <samantha.puth@gmail.com>
2014-09-20 15:13:26 -0700

    Created Header-Footer Layout

---
+++ client/src/views/AppView.js
@@ -0,0 +1,38 @@
+define(function(require, exports, module) {
+  var View            = require('famous/core/View');
+  var Surface         = require('famous/core/Surface');
+  var Modifier        = require('famous/core/Modifier');
+  var Transform       = require('famous/core/Transform');
+  var StateModifier   = require('famous/modifiers/StateModifier');
+  var Easing          = require('famous/transitions/Easing');
+  var Transitionable  = require('famous/transitions/Transitionable');
+  var GenericSync     = require('famous/inputs/GenericSync');
+  var MouseSync       = require('famous/inputs/MouseSync');
+  var TouchSync       = require('famous/inputs/TouchSync');
+  GenericSync.register({'mouse': MouseSync, 'touch': TouchSync});
+
+  var PageView = require('views/PageView');
+
+  function AppView() {
+  	View.apply(this, arguments);
+
+  	_createPageView.call(this);
+  }
+
+  AppView.prototype = Object.create(View.prototype);
+  AppView.prototype.constructor = AppView;
+
+  AppView.DEFAULT_OPTIONS = {};
+
+  function _createPageView() {
+  	this.pageView = new PageView();
+  	this.pageModifier = new Modifier({
+  		Transform: function() {
+  			return Transform.translate(this.pageViewPos.get(), 0, 0);
+  		}.bind(this)
+  	});
+  	this._add(this.pageModifier).add(this.pageView);
+  }
+
+  module.exports = AppView;
+})

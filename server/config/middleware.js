8331c495d6b6751d9a0bc892faaf69566238c9ab
jimjea <jimjea@gmail.com>
2014-09-20 15:41:28 -0700

    added sports route, wildcard handler

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,5 +5,16 @@ module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(express.static(__dirname + '/../../client'));
 
+  // create express router for each API
+  var sportsSchedule = express.Router();
+
+  // define which routes are assigned to each route
+  app.use('/sports', sportsSchedule);
+
+  // wlidcard requests get routed to home
+  app.get('/*', function(req, res) {
+    res.redirect('/');
+  });
+
   
 }

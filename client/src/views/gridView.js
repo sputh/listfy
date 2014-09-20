62990431d74a6499378afd312cd37782fa4d5d92
sputh <samantha.puth@gmail.com>
2014-09-20 15:42:20 -0700

    Create file for GridView

---
+++ client/src/views/gridView.js
@@ -0,0 +1,27 @@
+var Engine = require("famous/core/Engine");
+var Surface = require("famous/core/Surface");
+var GridLayout = require("famous/views/GridLayout");
+
+var mainContext = Engine.createContext();
+
+var grid = new GridLayout({
+ dimensions: [3, 2]
+});
+
+var surfaces = [];
+grid.sequenceFrom(surfaces);
+
+for(var i = 0; i < 8; i++) {
+ surfaces.push(new Surface({
+ content: "panel " + (i + 1),
+ size: [undefined, undefined],
+ properties: {
+ backgroundColor: "hsl(" + (i * 360 / 8) + ", 100%, 50%)",
+ color: "#404040",
+ lineHeight: '200px',
+ textAlign: 'center'
+ }
+ }));
+}
+
+mainContext.add(grid);

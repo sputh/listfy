0903176c44c69052de2e37597e7effc0d4450c71
jimjea <jimjea@gmail.com>
2014-09-20 17:07:33 -0700

    added comments to sportsroutes

--- server/sports/sportsRoutes.js
+++ server/sports/sportsRoutes.js
@@ -3,6 +3,6 @@ var sportsController = require('./sportsController');
 // exporting to middleware and invoked using express route
 module.exports = function(app) {
 
-  app.get('/', sportsController.test);
+  app.get('/', sportsController.test); // delete later, just a test
 
 };

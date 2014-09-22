93d0028b9dbb1ea7b44a4da4da180df8c2f899de
jimjea <jimjea@gmail.com>
2014-09-22 14:07:24 -0700

    cleaned up nfl cron job, added comments

--- cron-jobs/nflfetcher.js
+++ cron-jobs/nflfetcher.js
@@ -24,48 +24,27 @@ var weeks = {
   '122': 14
 };
 
+// When cron job runs, it will check the current date and return the associated week
 var date = new Date();
 var week = (date.getMonth() + 1).toString() + (date.getDate() + 1).toString();
 
-// runs job every Tuesday at 2am
-// new cron('00 00 2 * * 2', function() {
-//   db.knex('nfl').truncate()
-//     .then(function() {
-//       NFL.getWeeklySchedule(weeks[week], function(err, schedule) {
-//         for (var i = 0; i < schedule.games.game.length; i++) {
-//           var weeklySchedule = new nflData({
-//             date: schedule.games.game[i].$.scheduled,
-//             hometeam: schedule.games.game[i].$.home,
-//             awayteam: schedule.games.game[i].$.away,
-//             channel: schedule.games.game[i].broadcast[0].$.network
-//           })
-//           weeklySchedule.save()
-//             .then(function(game) {
-//               console.log('saved properly');
-//             })
-//         }
-//       });
-//     })
-// }, null, true, "America/Los_Angeles");
-
-function test() {
+runs job every Tuesday at 2am
+new cron('00 00 2 * * 2', function() {
   db.knex('nfl').truncate()
-  .then(function() {
-    NFL.getWeeklySchedule(weeks[week], function(err, schedule) {
-      for (var i = 0; i < schedule.games.game.length; i++) {
-        var weeklySchedule = new nflData({
-          date: schedule.games.game[i].$.scheduled,
-          hometeam: schedule.games.game[i].$.home,
-          awayteam: schedule.games.game[i].$.away,
-          channel: schedule.games.game[i].broadcast[0].$.network
-        })
-        weeklySchedule.save()
-          .then(function(game) {
-            console.log('saved properly');
+    .then(function() {
+      NFL.getWeeklySchedule(weeks[week], function(err, schedule) {
+        for (var i = 0; i < schedule.games.game.length; i++) {
+          var weeklySchedule = new nflData({
+            date: schedule.games.game[i].$.scheduled,
+            hometeam: schedule.games.game[i].$.home,
+            awayteam: schedule.games.game[i].$.away,
+            channel: schedule.games.game[i].broadcast[0].$.network
           })
-      }
-    });
-  })
-}
-
-test();
+          weeklySchedule.save()
+            .then(function(game) {
+              console.log('saved properly');
+            })
+        }
+      });
+    })
+}, null, true, "America/Los_Angeles");

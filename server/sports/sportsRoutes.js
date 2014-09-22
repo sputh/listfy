1a82837771ecbf314a92ebc686474db97eb52c1e
jimjea <jimjea@gmail.com>
2014-09-21 23:57:46 -0700

    cleaned up deleted files

--- server/sports/sportsRoutes.js
+++
@@ -1,8 +0,0 @@
-var sportsController = require('./sportsController');
-
-// exporting to middleware and invoked using express route
-module.exports = function(app) {
-
-  app.get('/', sportsController.test); // delete later, just a test
-
-};

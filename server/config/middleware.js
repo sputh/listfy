de265d7172db4f4e2e164f016a67ffd8e271e3d1
jimjea <jimjea@gmail.com>
2014-09-23 12:21:48 -0700

    added routing to middleware for mlb

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -8,11 +8,13 @@ module.exports = function(app, express) {
   // create express router for each API
   var nflSchedule = express.Router();
   var ncaafSchedule = express.Router();
+  var mlbSchedule = express.Router();
   // ***** ADD new router here
 
   // define which routes are assigned to each route
   app.use('/nfl', nflSchedule);
   app.use('/ncaaf', ncaafSchedule);
+  app.use('/mlb', mlbSchedule);
   // ***** ADD base route for each API here
 
 
@@ -22,7 +24,8 @@ module.exports = function(app, express) {
   });
 
   // invoke exported router using express router for each individual API
-  require('../nfl/nflRoutes.js')(nflSchedule);
+  require('../nfl/nflRoutes')(nflSchedule);
   require('../ncaaf/ncaafRoutes')(ncaafSchedule);
+  require('../mlb/mlbRoutes')(mlbSchedule);
   // ***** ADD controller and route files and export them here
 };
--- server/mlb/mlbRoutes.js
+++
@@ -1,1 +0,0 @@
-mlbRoutes.js
